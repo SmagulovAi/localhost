4d4eb9d22b149e2efff6092c109dd287cedc0945
SmagulovAi <kzpvl92@mail.ru>
2023-03-19 11:55:09 +0600

    start

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col">
+                <form action="">
+                    <div class="mb-3">
+                        <label for="number1" class="form-label">Number 1</label>
+                        <input type="number" class="form-control" id="number1" name="number1">
+                    </div>
+                    <div class="mb-3">
+                        <label for="number2" class="form-label">Number 2</label>
+                        <input type="number" class="form-control" id="number2" name="number2">
+                    </div>
+                    <div class="mb3">
+                        <button class="btn btn-primary" type="submit">Result</button>
+                    </div>
+                    <div class="mb-3 mt-2">
+                        <label for="number" class="form-label">Result</label>
+                        <input type="number" class="form-control" id="result" name="Result">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
